3a220ce38060978f21851761f339a8622809a03a
Renzo Vandingenen <renzo.vandingenen@student.ehb.be>
2024-11-13 11:58:45 +0100

    generative design cirkels en vierkanten

--- generativedesign/generativedesign.js
+++ generativedesign/generativedesign.js
@@ -28,3 +28,41 @@ toteAfbeelding.onload = function () {
   // teken de afbeelding van de totebag gecentreerd en op de juiste schaal
   context.drawImage(toteAfbeelding, toteX, toteY, toteBreedte, toteHoogte);
 };
+
+// instellen kleur
+let basisKleur = Utils.randomNumber(0, 360);
+let verzadiging = 70;
+let lichtheid = 50;
+
+// marges instellen voor ontwerpgebied binnen de totebag
+let ontwerpStartX = toteX + toteBreedte * 0.3; // 30% marge van links
+let ontwerpEindX = toteX + toteBreedte * 0.7; // 30% marge van rechts
+let ontwerpStartY = toteY + toteHoogte * 0.5; // 50% marge van de bovenkant
+let ontwerpEindY = toteY + toteHoogte * 0.8; // 20% marge van de onderkant
+
+/**
+ * teken een cirkel met opgegeven parameters
+ * @param {number} x
+ * @param {number} y
+ * @param {number} straal
+ * @param {string} kleur
+ */
+function tekenCirkel(x, y, straal, kleur) {
+  context.fillStyle = kleur;
+  context.strokeStyle = "black";
+  Utils.fillAndStrokeCircle(x, y, straal);
+}
+
+/**
+ * teken een vierkant met opgegeven parameters
+ * @param {number} x
+ * @param {number} y
+ * @param {number} grootte
+ * @param {string} kleur
+ */
+function tekenVierkant(x, y, grootte, kleur) {
+  context.fillStyle = kleur;
+  context.strokeStyle = "black";
+  context.fillRect(x, y, grootte, grootte);
+  context.strokeRect(x, y, grootte, grootte);
+}
