5980dd8b21708dd24e6f83d3184aff8c083a8cc1
Renzo Vandingenen <renzo.vandingenen@student.ehb.be>
2024-12-14 18:03:41 +0100

    interactive design 2

--- interactivedesign/interactivedesign.js
+++ interactivedesign/interactivedesign.js
@@ -1,11 +1,9 @@
 "use strict";
 import context from "../script/context.js";
-import * as Utils from "../script/utils.js";
 
 let canvasWidth = context.canvas.width;
 let canvasHeight = context.canvas.height;
 
-// Helper functions
 function getRandom(min, max) {
   return Math.random() * (max - min) + min;
 }
@@ -13,3 +11,22 @@ function getRandom(min, max) {
 function getHSL(hue, saturation, lightness) {
   return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
 }
+
+class Star {
+  constructor(x, y, size, color) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.color = color;
+  }
+
+  draw() {
+    context.fillStyle = this.color;
+    context.beginPath();
+    context.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    context.fill();
+  }
+}
+
+let star = new Star(100, 100, getRandom(-10, 30), "white");
+star.draw();
